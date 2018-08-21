4c6739a860988aabd5aceb8f402f4346edf5c54c
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-08-21 11:08:47 +0200

    intégration de bootstrap et amélioration du css

--- index.php
+++ index.php
@@ -1,20 +1,30 @@
 <?php
     session_start();//Démarrage de la session
 
-    $_SESSION['firstName'] = 'Fabien';//Déclaration session firstName
-    $_SESSION['lastName'] = 'Lamotte';//Déclaration session lastName
-    $_SESSION['age'] = 32;//Déclaration session age
+    $_SESSION['firstName'] = 'Fabien';//Déclaration session firstName avec valeur 'Fabien'
+    $_SESSION['lastName'] = 'Lamotte';//Déclaration session lastName avec valeur 'Lamotte'
+    $_SESSION['age'] = 32;//Déclaration session age avec valeur 32
 ?>
 <!DOCTYPE html>
 <html lang="foreach" dir="ltr">
   <head>
     <meta charset="utf-8" />
-    <title>Exercice 2</title>
     <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>Exercice 2</title>
   </head>
   <body>
-      <p>
-          <a href="secondPage.php" class="link">Cliquer ici</a><!--Lien vers la seconde page où seront transmis les valeurs sessions-->
-      </p>
+      <div class="container">
+          <div class="row">
+              <div class="offset-3 col-6 info">
+                <p>
+                    <a href="secondPage.php" class="link">Cliquer ici pour recevoir vos informations</a><!--Lien vers la seconde page où seront transmis les valeurs sessions-->
+                </p>
+              </div>
+          </div>
+      </div>
   </body>
 </html>
--- secondPage.php
+++ secondPage.php
@@ -4,14 +4,24 @@ session_start();//Démarrage de la session
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
-    <meta charset="utf-8" />
-    <title>Exercice2</title>
+   <meta charset="utf-8" />
     <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>Exercice 2</title>
   </head>
   <body>
-    <p><!--Affichage avec phrase concaténée comprenant les paramètres de la page index.php-->
-      Je m'appelle <?php echo $_SESSION['firstName'] . ' ' . $_SESSION['lastName']; ?> et j'ai <?php echo $_SESSION['age']; ?> ans  !
-      <a href="index.php" class="link">Retour</a>
-    </p>
+    <div class="container">
+          <div class="row">
+              <div class="offset-3 col-6 givenInfo">
+                <p><!--Affichage avec phrase concaténée comprenant les paramètres de la page index.php-->
+                  Je m'appelle <?php echo $_SESSION['firstName'] . ' ' . $_SESSION['lastName']; ?> et j'ai <?php echo $_SESSION['age']; ?> ans LOL !
+                  <a href="index.php" class="link">Retour</a>
+                </p>
+              </div>
+          </div>
+      </div>
   </body>
 </html>
